e82738f0a4cf21bb23d3e1c384e30218931a38b5
Keewis <keewis@posteo.de>
2021-06-05 01:36:13 +0200

    don't modify the options

--- sphinx_autosummary_accessors/autosummary.py
+++ sphinx_autosummary_accessors/autosummary.py
@@ -20,12 +20,11 @@ def extract_documenter(content):
 def create_documenter_from_template(autosummary, app, obj, parent, full_name):
     real_name = ".".join(full_name.split("::"))
 
-    options = autosummary.options.copy()
-    template_name = options.pop("template", None)
+    options = autosummary.options
+    template_name = options.get("template", None)
     if template_name is None:
         return original_create_documenter(autosummary, app, obj, parent, full_name)
 
-    options.pop("toctree", None)
     options["imported_members"] = options.get("imported_members", False)
     options["recursive"] = options.get("recursive", False)
 
