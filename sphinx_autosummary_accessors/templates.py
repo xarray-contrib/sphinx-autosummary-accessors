01ff75f6f2c5ed9c703384b7d462004539a49164
Keewis <keewis@posteo.de>
2022-04-02 20:25:34 +0200

    actually find all templates

--- sphinx_autosummary_accessors/templates.py
+++ sphinx_autosummary_accessors/templates.py
@@ -2,5 +2,5 @@ import pathlib
 
 root = pathlib.Path(__file__).parent / "templates"
 
-known_templates = [path.name for path in root.glob("*.rst")]
+known_templates = [path.name for path in root.glob("**/*.rst")]
 templates_path = str(root)
