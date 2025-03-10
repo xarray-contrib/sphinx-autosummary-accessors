b882e7893b70a62b45f37300bd6f29a4685c740c
Justus Magin <keewis@posteo.de>
2025-03-10 12:17:24 +0100

    stay backwards-compatible

--- sphinx_autosummary_accessors/autosummary.py
+++ sphinx_autosummary_accessors/autosummary.py
@@ -1,5 +1,7 @@
 import re
 
+import sphinx
+from packaging import version
 from sphinx.ext.autosummary import Autosummary, generate
 
 from sphinx_autosummary_accessors.templates import known_templates
@@ -19,36 +21,74 @@ def extract_documenter(content):
     return directive_name, "::".join([modname, name])
 
 
-def create_documenter_from_template(autosummary, obj, parent, full_name, *, registry):
-    real_name = ".".join(full_name.split("::"))
+if version.parse(sphinx.__version__) >= version.Version("8.2.0"):
 
-    options = autosummary.options
-    template_name = options.get("template", None)
-    if template_name is None or template_name not in known_templates:
-        return original_create_documenter(
-            autosummary, obj, parent, full_name, registry=registry
+    def create_documenter_from_template(
+        autosummary, obj, parent, full_name, *, registry
+    ):
+        real_name = ".".join(full_name.split("::"))
+
+        options = autosummary.options
+        template_name = options.get("template", None)
+        if template_name is None or template_name not in known_templates:
+            return original_create_documenter(
+                autosummary, obj, parent, full_name, registry=registry
+            )
+
+        imported_members = options.get("imported_members", False)
+        recursive = options.get("recursive", False)
+
+        context = {}
+        context.update(autosummary.app.config.autosummary_context)
+
+        rendered = generate.generate_autosummary_content(
+            real_name,
+            obj,
+            parent,
+            template=generate.AutosummaryRenderer(autosummary.app),
+            template_name=template_name,
+            context=context,
+            imported_members=imported_members,
+            recursive=recursive,
+            registry=registry,
         )
 
-    imported_members = options.get("imported_members", False)
-    recursive = options.get("recursive", False)
+        documenter_name, real_name = extract_documenter(rendered)
+        doccls = registry.documenters.get(documenter_name)
+        documenter = doccls(autosummary.bridge, real_name)
 
-    context = {}
-    context.update(autosummary.app.config.autosummary_context)
+        return documenter
 
-    rendered = generate.generate_autosummary_content(
-        real_name,
-        obj,
-        parent,
-        template=generate.AutosummaryRenderer(autosummary.app),
-        template_name=template_name,
-        context=context,
-        imported_members=imported_members,
-        recursive=recursive,
-        registry=registry,
-    )
+else:
 
-    documenter_name, real_name = extract_documenter(rendered)
-    doccls = registry.documenters.get(documenter_name)
-    documenter = doccls(autosummary.bridge, real_name)
+    def create_documenter_from_template(autosummary, app, obj, parent, full_name):
+        real_name = ".".join(full_name.split("::"))
 
-    return documenter
+        options = autosummary.options
+        template_name = options.get("template", None)
+        if template_name is None or template_name not in known_templates:
+            return original_create_documenter(autosummary, app, obj, parent, full_name)
+
+        imported_members = options.get("imported_members", False)
+        recursive = options.get("recursive", False)
+
+        context = {}
+        context.update(app.config.autosummary_context)
+
+        rendered = generate.generate_autosummary_content(
+            real_name,
+            obj,
+            parent,
+            template=generate.AutosummaryRenderer(app),
+            template_name=template_name,
+            app=app,
+            context=context,
+            imported_members=imported_members,
+            recursive=recursive,
+        )
+
+        documenter_name, real_name = extract_documenter(rendered)
+        doccls = app.registry.documenters.get(documenter_name)
+        documenter = doccls(autosummary.bridge, real_name)
+
+        return documenter
